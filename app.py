9cdc8db670067ce62a8019a677359fc89337fc3d
mtanzella <g1pd78@gmail.com>
2023-06-19 19:22:06 +0300

    stacked widget

--- app.py
+++ app.py
@@ -14,54 +14,104 @@ from PyQt6.QtWidgets import (
     QMainWindow, 
     QMenu, 
     QRadioButton,
+    QStackedWidget,
 )
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.initGUI()
-
-    def initGUI(self):
-        # area for scrolling option
-        self.scroll = QScrollArea()
-
         self.resize(900, 800)
         self.setWindowTitle("Anki Card Generator")
 
-        # 
+        self.stacked_widget = QStackedWidget()
+        self.scroll_area1 = QScrollArea()
+        self.scroll_area2 = QScrollArea()
 
-    
-        self.label = QLabel("Введите слово или словосочетание для поиска:")
-        self.input = QLineEdit()
-        self.button = QPushButton("Поиск!")
+        self.setup_ui()
 
-        layout = QGridLayout()
+
+    #def initGUI(self):
+
+
+
+        # change settings for scrolling
+        #self.scroll1.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        #self.scroll1.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+        #self.scroll1.setWidgetResizable(True)
+
+      
+
+        # setting the central widget to show it 
+        #self.setCentralWidget(self.scroll)
+
+    def setup_ui(self):
+        central_widget = QWidget()
+        layout = QVBoxLayout(central_widget)
+
+        widget1 = QWidget()
+        layout1 = self.initLayout1(widget1)
+        self.scroll_area1.setWidget(widget1)
+
+        widget2 = QWidget()
+        layout2 = self.initLayout2(widget2)
+        self.scroll_area2.setWidget(widget2)
+
+        self.stacked_widget.addWidget(self.scroll_area1)
+        self.stacked_widget.addWidget(self.scroll_area2)
+
+        layout.addWidget(self.stacked_widget)
+        self.setCentralWidget(central_widget)
+
+        button1 = QPushButton("Scroll Area 1")
+        button2 = QPushButton("Scroll Area 2")
+        layout.addWidget(button1)
+        layout.addWidget(button2)
+
+        # Подключаем кнопки к слотам переключения scroll area
+        button1.clicked.connect(self.show_scroll_area1)
+        button2.clicked.connect(self.show_scroll_area2)
+
+    def show_scroll_area1(self):
+        self.stacked_widget.setCurrentWidget(self.scroll_area1)
+
+    def show_scroll_area2(self):
+        self.stacked_widget.setCurrentWidget(self.scroll_area2)
+
+    def initLayout1(self, widget):
+        label = QLabel("Введите слово или словосочетание для поиска:")
+        input = QLineEdit()
+        button = QPushButton("Поиск!")
+ 
+        layout = QGridLayout(widget)
         layout.setContentsMargins(40, 40, 40, 40)
         layout.setSpacing(10)
-        layout.addWidget(self.input, 1, 0)
-        layout.addWidget(self.label, 0, 0)
-        layout.addWidget(self.button, 1, 1)
+        layout.addWidget(input, 1, 0)
+        layout.addWidget(label, 0, 0)
+        layout.addWidget(button, 1, 1)
 
         for i in range(35):
-            self.radio = QRadioButton(f"Банк, у которого был «актив» (в форме займа клиенту) в размере $100,000 в июне, к июлю может получить ноль.{i}", self)
-            self.radio.toggled.connect(self.showDetails)
-            layout.addWidget(self.radio, i+2, 0)
+            radio = QRadioButton(f"Банк, у которого был «актив» (в форме займа клиенту) в размере $100,000 в июне, к июлю может получить ноль.{i}", self)
+            radio.toggled.connect(self.showDetails)
+            layout.addWidget(radio, i+2, 0)
+
+        return layout
 
         # if last one checked - create another one lineedit for my own text
 
-        # change settings for scrolling
-        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.scroll.setWidgetResizable(True)
+    def initLayout2(self, widget):
+        label = QLabel("Введите слово или словосочетание для поиска:")
+        input = QLineEdit()
+        button = QPushButton("Поиск!")
 
-        # create a widget to set it as central
-        container = QWidget()
-        container.setLayout(layout)
-        self.scroll.setWidget(container)
+        layout = QGridLayout(widget)
+        layout.setContentsMargins(40, 40, 40, 40)
+        layout.setSpacing(10)
+        layout.addWidget(input, 1, 0)
+        layout.addWidget(label, 0, 0)
+        layout.addWidget(button, 1, 1)
 
-        # setting the central widget to show it 
-        self.setCentralWidget(self.scroll)
+        return layout
 
 
     def show_state(self, s):
